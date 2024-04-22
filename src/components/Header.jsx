5e64d9950d5e77e110aa8628d4ac6db5f967b8f0
Francisco <fazeopticdiaz@gmail.com>
2024-04-22 18:05:42 -0500

    NavBar and SearchBar

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <>
+      <header>
+        <nav className="nav-bar">
+          <a className="link" target="_blank">
+            Fylm
+          </a>
+          <a>
+            <img src="icon.png"></img>
+          </a>
+        </nav>
+      </header>
+    </>
+  );
+}
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+export default function SearchBar() {
+  return (
+    <>
+      <form>
+        <input
+          className="form"
+          type="text"
+          placeholder="   Search for a movie here..."
+        />
+      </form>
+    </>
+  );
+}
