de06b59796de584ec5a05e4e7ec6ec91890e1b81
MrCheks <fazeopticdiaz@gmail.com>
2024-04-22 23:09:11 -0500

    Updating Nav-Bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,20 @@
 export default function Header() {
   return (
     <>
-      <header>
-        <nav className="nav-bar">
-          <a className="link" target="_blank">
-            Fylm
-          </a>
-          <a>
-            <img src="icon.png"></img>
-          </a>
-        </nav>
-      </header>
+      <nav>
+        <ul className="nav-bar">
+          <li>
+            <a className="link" target="_blank">
+              Fylm
+            </a>
+          </li>
+          <li>
+            <a>
+              <img src="icon.png"></img>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
