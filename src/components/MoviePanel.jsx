24f637142d0a283629ebe96e4b3121efb1245833
MrCheks <fazeopticdiaz@gmail.com>
2024-04-25 18:06:47 -0500

    Panel Update, CSS for ErrorAlert

--- src/components/MoviePanel.jsx
+++ src/components/MoviePanel.jsx
@@ -9,11 +9,21 @@ export default function MoviePanel({ movies }) {
               src={movie.Poster !== "N/A" ? movie.Poster : "image-missing.svg"}
             ></img>
             <div className="movie-details">
-              <h2>{movie.Title}</h2>
-              {movie.Plot !== "N/A" ? <p>{movie.Plot}</p> : "No Plot Found"}
-              <p>Rated: {movie.Rated}</p>
-              <p>Released {movie.Released}</p>
-              <p>Runtime: {movie.Runtime}</p>
+              <h2 className="title">{movie.Title}</h2>
+              <br />
+              <p className="description">
+                Description
+                <br />
+              </p>
+              <p className="plot">
+                {movie.Plot !== "N/A" ? <p>{movie.Plot}</p> : "No Plot Found"}
+              </p>
+              <br />
+              <div className="rrr">
+                <p>Rated: {movie.Rated}</p>
+                <p>Released {movie.Released}</p>
+                <p>Runtime: {movie.Runtime}</p>
+              </div>
             </div>
           </div>
         ))}
